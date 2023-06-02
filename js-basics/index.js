5df0d4569b40bcb800b6ba662edc01c1177c2819
Michael Anyadioha <m.anyadioha@yahoo.com>
2023-06-02 16:04:03 +0100

    Testing

---
+++ js-basics/index.js
@@ -0,0 +1,12 @@
+
+// Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+// Calculate a value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
